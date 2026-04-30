865776d562c0a2fc8253893ada5f854ed68b817c
Patrick Jo <patrickprada2002@gmail.com>
2026-04-30 11:56:26 -0300

    realizando cambios en login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { apiFetch } from "./api";
 
 export default function Login({ onSuccess }) {
-  const [email, setEmail] = useState("admin@gym.com");
-  const [password, setPassword] = useState("Admin123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
